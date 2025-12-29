bbc072cfa00b0415cd1cb9a7aedea4fa5b0ad47c
eynuts <candari.arvin@gmail.com>
2025-12-29 17:53:37 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-// src/main.jsx
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './pages/App.jsx'; // <-- fixed path
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>
-);
+  </StrictMode>,
+)
