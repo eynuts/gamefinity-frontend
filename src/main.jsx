91e634317712e300cfea7973f74d0f541f7afb2b
eynuts <candari.arvin@gmail.com>
2025-12-29 17:38:56 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,31 @@
-import { StrictMode } from 'react'
+// src/main.jsx
+import { StrictMode, useEffect } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+// Function to set the favicon dynamically
+function setFavicon(url) {
+  let link = document.querySelector("link[rel*='icon']");
+  if (!link) {
+    link = document.createElement('link');
+    link.rel = 'icon';
+    document.getElementsByTagName('head')[0].appendChild(link);
+  }
+  link.href = url;
+}
+
+// Wrapper component to set favicon on mount
+function Root() {
+  useEffect(() => {
+    setFavicon('/favicon.ico'); // Path to your icon in public folder
+    document.title = 'Gamefinity'; // Optional: Set tab title
+  }, []);
+
+  return <App />;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Root />
+  </StrictMode>
+);
